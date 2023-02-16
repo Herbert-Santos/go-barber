d34b4c718b56c29093100f27087222e6ce09aa8c
Herbert-Santos <hebertesantos123@gmail.com>
2023-02-16 20:48:00 -0300

    Validando data

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,17 +1,33 @@
 import { Router } from 'express';
 import { v4 as uuidv4 } from 'uuid';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointments {
+    id: string;
+    provider: string;
+    date: Date;
+}
+
+const appointments: Appointments[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
     const { provider, date } = request.body;
 
+    const parsedDate = startOfHour(parseISO(date));
+    const findAppointmentsInSameDate = appointments.find(appointment => 
+        isEqual(parsedDate, appointment.date),
+    );
+
+    if(findAppointmentsInSameDate) {
+        return response.status(400).json({message:'This appointment is already booked!'});
+    }
+
     const appointment = {
         id: uuidv4(),
         provider,
-        date
+        date: parsedDate,
     };
 
     appointments.push(appointment);
