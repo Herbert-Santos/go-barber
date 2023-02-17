0e5c9d1245aac92d658d45214619e70b25a64a26
Herbert-Santos <hebertesantos123@gmail.com>
2023-02-16 21:27:28 -0300

    Criando entidade de agendamentos

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,16 +1,11 @@
 import { Router } from 'express';
-import { v4 as uuidv4 } from 'uuid';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
+
+import Appointment from './models/appointment';
 
 const appointmentsRouter = Router();
 
-interface Appointments {
-    id: string;
-    provider: string;
-    date: Date;
-}
-
-const appointments: Appointments[] = [];
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
     const { provider, date } = request.body;
@@ -24,11 +19,7 @@ appointmentsRouter.post('/', (request, response) => {
         return response.status(400).json({message:'This appointment is already booked!'});
     }
 
-    const appointment = {
-        id: uuidv4(),
-        provider,
-        date: parsedDate,
-    };
+    const appointment = new Appointment(provider, parsedDate);
 
     appointments.push(appointment);
     return response.json(appointment);
