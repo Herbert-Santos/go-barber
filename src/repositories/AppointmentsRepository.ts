46207305a6082d0ecd74f769b8eeb1c81ac23f51
Herbert-Santos <hebertesantos123@gmail.com>
2023-02-16 22:42:31 -0300

    Trabalhando com dados

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,6 +1,10 @@
 import { isEqual } from 'date-fns';
 import Appointment from "../models/appointment";
 
+interface CreateAppointmentDTO {
+    provider: string,
+    date: Date,
+}
 
 class AppointmentsRepository {
     private appointments: Appointment[];
@@ -13,17 +17,17 @@ class AppointmentsRepository {
         return this.appointments;
     }
 
-    public findByDate(date: Date): Appointment | null {
-        const findAppointments = this.appointments.find(appointment =>
-            isEqual(date, appointment.date),
+    public findByDate( date: Date ): Appointment | null {
+        const findAppointments = this.appointments.find( appointment =>
+            isEqual( date, appointment.date ),
         );
         return findAppointments || null;
     }
+//provider: string, date: Date
+    public create({ provider, date }: CreateAppointmentDTO ): Appointment {
+        const appointment = new Appointment({ provider, date });
 
-    public create(provider: string, date: Date): Appointment {
-        const appointment = new Appointment(provider, date);
-
-        this.appointments.push(appointment);
+        this.appointments.push( appointment );
         return appointment;
     }
 }
